af44cffd36e8fb5d73fff15af646ce8b266a347e
Junshen18 <junshenhere@gmail.com>
2025-04-05 02:28:50 +0800

    add collections section

--- packages/thirdweb-app/src/app/page.tsx
+++ packages/thirdweb-app/src/app/page.tsx
@@ -3,12 +3,14 @@ import Hero from "@/components/landing-page/Hero";
 import About from "@/components/landing-page/About";
 import Story from "@/components/landing-page/Story";
 import Rarity from "@/components/landing-page/Rarity";
+import Collections from "@/components/landing-page/Collections";
 export default function Home() {
   return <div>
     <Navigation />
     <Hero />
     <About />
     <Story />
+    <Collections />
     <Rarity />
   </div>;
 }
