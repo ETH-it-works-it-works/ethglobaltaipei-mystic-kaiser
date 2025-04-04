0c9d03ca5d1b2b0c9c44ebac2ce8061254da7441
Junshen18 <junshenhere@gmail.com>
2025-04-05 01:51:22 +0800

    add rarity section

--- packages/thirdweb-app/src/app/page.tsx
+++ packages/thirdweb-app/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Navigation from "@/components/landing-page/Navigation";
 import Hero from "@/components/landing-page/Hero";
 import About from "@/components/landing-page/About";
+import Story from "@/components/landing-page/Story";
+import Rarity from "@/components/landing-page/Rarity";
 export default function Home() {
   return <div>
     <Navigation />
     <Hero />
     <About />
+    <Story />
+    <Rarity />
   </div>;
 }
