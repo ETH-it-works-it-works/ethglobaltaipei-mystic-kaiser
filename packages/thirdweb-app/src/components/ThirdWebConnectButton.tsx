3fcaf1acedf86994096c50d35dbc18bfd68217a2
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-04-05 09:46:49 +0800

    added third web button

--- packages/thirdweb-app/src/components/ThirdWebConnectButton.tsx
+++ packages/thirdweb-app/src/components/ThirdWebConnectButton.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { ConnectButton } from "thirdweb/react";
-// import { useThirdWeb } from "@/hooks/useThirdWeb";
+import { useThirdWeb } from "@/hooks/useThirdWeb";
 
 export default function ThirdWebConnectButton() {
-  //   const { client, appMetadata, accountAbstraction } = useThirdWeb();
+  const { client, appMetadata, accountAbstraction } = useThirdWeb();
   return (
     <React.Fragment>
-      {/* <ConnectButton
+      <ConnectButton
         client={client}
         appMetadata={appMetadata}
         accountAbstraction={accountAbstraction}
-      /> */}
+      />
     </React.Fragment>
   );
 }
